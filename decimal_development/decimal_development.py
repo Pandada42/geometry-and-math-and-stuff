d456a0ec514c0df499e913164b170ed648ebfd91
Pandada <mb692906@gmail.com>
2023-03-07 20:09:54 +0100

    Wait, what just happened ?

---
+++ decimal_development/decimal_development.py
@@ -0,0 +1,12 @@
+import numpy
+import matplotlib.pyplot as plt
+
+
+def decimal_development(n : int):
+    """
+    :param n: number to return the decimal development of the inverse
+    :type n: int
+    """
+
+    development = ""
+
